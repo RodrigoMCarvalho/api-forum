435a0d303473aaa2afcc4253f0b13fc4b3a3a27b
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-10-05 20:53:36 -0300

    testes

---
+++ src/test/java/com/rodrigo/forum/repository/CursoRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.rodrigo.forum.repository;
+
+import com.rodrigo.forum.model.Curso;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class CursoRepositoryTest {
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @Autowired
+    private TestEntityManager em;
+
+    @Test
+    void deveBuscarCursoPorNome() {
+        String nomeCurso = "HTML 5";
+
+        Curso html5 = new Curso();
+        html5.setNome(nomeCurso);
+        html5.setCategoria("Programacao");
+        em.persist(html5);
+
+        Curso curso = cursoRepository.findByNome(nomeCurso);
+
+        Assert.assertNotNull(curso);
+        Assert.assertEquals(nomeCurso, curso.getNome());
+    }
+
+    @Test
+    void naoDeveCarregarCursoInexistente() {
+        String nomeCurso = "JPA";
+        Curso curso = cursoRepository.findByNome(nomeCurso);
+
+        Assert.assertNull(curso);
+    }
+}
