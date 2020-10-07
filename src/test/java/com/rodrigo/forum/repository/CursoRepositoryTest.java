c92d7fc07af2ccf4436ea183e34b9ccb16b74b06
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-10-07 15:58:45 -0300

    testando com mockmvc do spring

--- src/test/java/com/rodrigo/forum/repository/CursoRepositoryTest.java
+++ src/test/java/com/rodrigo/forum/repository/CursoRepositoryTest.java
@@ -3,12 +3,15 @@ package com.rodrigo.forum.repository;
 import com.rodrigo.forum.model.Curso;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
